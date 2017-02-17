bc5baaf2f574393eb54ef67741796018fa363965
Saswat Padhi <padhi@cs.ucla.edu>
2017-02-17 11:57:15 -0800

    Install good LaTeX packages for scientific writing

---
+++ install_latex_packages.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+which tlmgr
+tlmgr install amsfonts babel caption cleveref graphics hyperref koma-script   \
+              listings lm mathtools microtype oberdiek pgf pgfplots psnfss    \
+              siunitx subfigure todonotes tools units xcolor
