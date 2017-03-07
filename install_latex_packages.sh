c5da19b3512185c20635d9409f9ff4d718d7fda7
Muhammad Ali Gulzar <gulzarmuhammadali@gmail.com>
2017-03-07 15:43:08 -0800

    Added new packages

--- install_latex_packages.sh
+++ install_latex_packages.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
 which tlmgr
-tlmgr install amsfonts \
-              babel \
+tlmgr install amsfonts amssymb alltt amsmath array \
+              babel bibentry booktabs \
               caption cleveref \
               DejaVuSansMono \
-              ec \
-              fixme \
-              graphics \
+              ec epsfig enumitem \
+              fixme fullpage fancyvrb filecontents float \
+              graphics graphicx \
               hyperref \
               inconsolata \
               koma-script \
               listings lm \
-              mathtools mdwtools microtype ms \
-              oberdiek \
-              pgf pgfplots psnfss \
+              mathtools mdwtools microtype ms multirow mdwlist \
+              normalem \
+              oberdiek override\
+              pgf pgfplots psnfss palatino proposal pifont \
               siunitx subfigure \
-              todonotes tools \
-              units upquote \
-              xcolor
+              todonotes tools table times \
+              units upquote url usenames \
+              verbatim \
+              wrapfig \
+              xcolor xspace
