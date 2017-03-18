ca413c67ad10bc9db477773fbed9ae9df3ca42dc
Saswat Padhi <padhi@cs.ucla.edu>
2017-03-18 02:56:19 -0700

    Install every damn thing

--- install_latex_packages.sh
+++ install_latex_packages.sh
@@ -2,28 +2,5 @@
 
 which tlmgr
 
-# One line per letter, lexicographically sorted within the line
-# ... makes it much easier to eye-ball the list.
-
-tlmgr install algorithms amsfonts amsmath \
-              babel booktabs \
-              caption cleveref \
-              DejaVuSansMono \
-              ec enumitem etoolbox \
-              fancyvrb filecontents fixme float footmisc \
-              graphics \
-              hyperref \
-              inconsolata \
-              koma-script \
-              latex listings lkproof lm \
-              mathtools mdwtools microtype ms multirow \
-              natbib \
-              oberdiek \
-              pgf pgfplots preprint psnfss \
-              relsize \
-              siunitx subfigure \
-              todonotes tools \
-              ulem units upquote url \
-              wrapfig \
-              xcolor \
-  || true
+tlmgr update --self --all || true
+tlmgr install scheme-full || true
